21dd09d0566321ea9ba37efa88bffc847e8543c7
HUGO LOPES DE SOUZA <24122289@dsn127m1242267.cfp127aluno.local>
2025-09-25 11:59:34 -0300

    subindo projeto

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,5 +1,6 @@
 import Input from './Input'
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function CardLogin() {
 	const [email, setEmail] = useState('')
@@ -18,12 +19,49 @@ export default function CardLogin() {
 			</div>
 
 			<div className="inputs">
-				<Input srcImage="/icons-email.svg" inputName="Email" type="email" value={email} onChange={setEmail} />
-				<Input srcImage="/icons-lock.svg" inputName="Senha" type="password" value={senha} onChange={setSenha} />
+				<Input
+					srcImage="/icons-email.svg"
+					inputName="Email"
+					placeholder="exemplo123@gmail.com"
+					type="email"
+					value={email}
+					onChange={setEmail}
+				/>
+				<Input
+					srcImage="/icons-lock.svg"
+					extImage="/icons-eye.svg"
+					extImageName="eye"
+					inputName="Senha"
+					placeholder="**********"
+					type="password"
+					value={senha}
+					onChange={setSenha}
+				/>
 			</div>
 
-			<div className="navigation_buttons">
-				<button onClick={handleSubmit}>Entrar</button>
+			<div className="login_opt">
+				<div className="remember_me">
+					<input type="checkbox" name="remember" id="remember_me" />
+					<label htmlFor="remember_me">Lembre-se de mim</label>
+				</div>
+				<div>
+					<Link href={'/'}>Esqueceu a senha?</Link>
+				</div>
+			</div>
+
+			<div className="divider">
+				<div className="line"></div>
+				<p>Or login with</p>
+				<div className="line"></div>
+			</div>
+
+			<div className="container_google_box">
+				<img src="/icons-google.svg" alt="google icon" />
+				<p>Google</p>
+			</div>
+
+			<div className="login_btn">
+				<button onClick={handleSubmit}>Login</button>
 			</div>
 		</div>
 	)
--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-
 interface InputProps {
 	srcImage: string
+	extImage?: string
+	extImageName?: string
 	inputName: string
+	placeholder: string
 	type?: string
 	value: string
 	onChange: (value: string) => void
 }
 
-export default function Input({ srcImage, inputName, type = 'text', value, onChange }: InputProps) {
+export default function Input({ srcImage, extImage, extImageName, inputName, placeholder, type = 'text', value, onChange }: InputProps) {
 	return (
 		<div className="input_container">
 			<img src={srcImage} alt={`${inputName} icon`} />
+			{extImage != undefined && <img src={extImage} alt={`${extImageName} icon`}></img>}
 			<label>
-				{inputName}
-				<input type={type} value={value} onChange={(e) => onChange(e.target.value)} />
+				<input type={type} value={value} placeholder={placeholder} onChange={(e) => onChange(e.target.value)} />
 			</label>
 		</div>
 	)
